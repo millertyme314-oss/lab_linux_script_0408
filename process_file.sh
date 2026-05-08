c951ae77af1c537adea96748f6b90ae8932c8225
Zeek Millz <228266305+millertyme314-oss@users.noreply.github.com>
2026-05-07 22:01:58 -0400

    let's see now

--- process_file.sh
+++ process_file.sh
@@ -29,7 +29,7 @@ archived_stuff="ARCHIVED: $(echo "$line" | tr '[:lower:]' '[:upper:]')"
 
 # Archive the processed line    
 echo "$processed_line" >> "$OUTPUT_FILE"
-echo "$processed_line" +="-arch" >> "$ARCHIVE"
+echo $processed_line+="_arc" >> "$ARCHIVE"
 
 done < "$INPUT_FILE"
 echo "Done!"
